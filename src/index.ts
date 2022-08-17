a73ea634f1414301489dd6a48f827f3b5aa2e80e
Henrique Ramos <henriquealberone@outlook.com>
2022-08-17 17:04:40 -0300

    fix: export defaults in index

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import AbstractPlugin from './shared/AbstractPlugin';
 
 export { AbstractPlugin as BasePlugin };
 
+export { default as Draggable } from './Draggable';
 export * from './Draggable';
-export * from './Droppable/Droppable';
+export { default as Droppable } from './Droppable';
+export * from './Droppable';
+export { default as Swappable } from './Swappable';
 export * from './Swappable';
+export { default as Sortable } from './Sortable';
 export * from './Sortable';
 
 export { Sensors, Plugins };
