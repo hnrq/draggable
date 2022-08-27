2325815c7dad588235f98c4d5137c0fc8f88b48e
Henrique Ramos <henriquealberone@outlook.com>
2022-08-27 03:08:53 -0300

    chore: format environment.ts

--- src/test-utils/helpers/environment.ts
+++ src/test-utils/helpers/environment.ts
@@ -1,22 +1,22 @@
-export function createSandbox(content) {
+export const createSandbox = (content) => {
   const sandbox = document.createElement('div');
   sandbox.innerHTML = content;
   document.body.appendChild(sandbox);
 
   return sandbox;
-}
+};
 
-export function withElementFromPoint(elementFromPoint, callback) {
+export const withElementFromPoint = (elementFromPoint, callback) => {
   const originalElementFromPoint = document.elementFromPoint;
   document.elementFromPoint = () => elementFromPoint;
   callback();
   document.elementFromPoint = originalElementFromPoint;
-}
+};
 
 export const REQUEST_ANIMATION_FRAME_TIMEOUT = 15;
 
-export function waitForRequestAnimationFrame(
+export const waitForRequestAnimationFrame = (
   requestAnimationFrameTimeout = REQUEST_ANIMATION_FRAME_TIMEOUT
-) {
+) => {
   vi.advanceTimersByTime(requestAnimationFrameTimeout + 1);
-}
+};
